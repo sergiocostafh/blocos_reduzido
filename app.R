a8747f10e03ce77b5da28686e3057cf88f8d621f
Sergio Vinicius Serejo da Costa Filho <sergio.costa@arauco.com>
2020-03-10 15:06:28 -0300

    UPDATE 10.03

--- app.R
+++ app.R
@@ -1,15 +1,14 @@
-#APP BACKUP SEM REATIVIDADE
-
 library(rgdal)
 library(leaflet)
 library(shiny)
 
-##Read the data
+# Load shapefiles
 blocos <- readOGR(dsn= getwd() , layer="blocos")
 blocos <- spTransform(blocos, CRS("+init=epsg:4326"))
 hortos <- readOGR(dsn= getwd() , layer="hortos")
 hortos <- spTransform(hortos, CRS("+init=epsg:4326"))
-#Create the objective vector with random values
+
+# Generate random values for the column 'habilita'
 blocos_nomes <- unique(blocos$BLOCO_L)
 habilita <- data.frame(nome=blocos_nomes,habilita=sample(c(TRUE,FALSE), length(blocos_nomes), replace = TRUE))
 blocos$habilita <- habilita$habilita[match(blocos$BLOCO_L,habilita$nome)]
@@ -22,25 +21,24 @@ server <- function(input, output, session) {
     
     output$map <- renderLeaflet({
         
-        # identificar o bloco clicado
+        # Identify the group of the clicked feature
         label <- input$map_shape_click$id
         label_idx <- blocos@data$UNIQUEID %in% label
         blc <- blocos@data$BLOCO_L[label_idx]
         blc_idx <- which(blocos@data$BLOCO_L%in%blc)
-        # atualizar coluna
+        # Update the value for the column 'habilita'
         if (any(blc_idx)) {
             blocos$habilita[blc_idx] <- !blocos$habilita[blc_idx]
             blocos <<- blocos
         }
         
-        #Labels
+        # Labels
         labels <- paste("Bloco: ", blocos@data$BLOCO_L,"<br/>", "Habilitado: ", blocos$habilita, sep="")
         
-        #Cores
-        pal <- colorFactor(palette = c('red','green'), levels = unique(blocos$habilita))
-        pal2 <- colorFactor(palette = c('black','red'), levels = unique(blocos$PIC2020))
+        # Colors
+        pal <- colorFactor(palette = c('red','green'), levels = c(FALSE,TRUE))
         
-        #Show map
+        # Show map
         leaflet(blocos) %>% 
             addProviderTiles(providers$CartoDB.Positron)  %>% 
             addPolygons(data=hortos,weight=2,fillOpacity = 0, color = 'black')%>%
@@ -50,7 +48,7 @@ server <- function(input, output, session) {
                 label = lapply(labels, htmltools::HTML),
                 weight=.3,
                 opacity = 1,
-                color=~pal2(PIC2020),
+                color='black',
                 highlightOptions = highlightOptions(weight = 5, fillOpacity = 1, bringToFront = TRUE)
             ) %>%
             addLegend(
